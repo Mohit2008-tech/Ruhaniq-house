b34beb8a1df9edb2af7b8073ee8a7088cda0d051
Mohit Aggarwal <83158393+Mohit2008-tech@users.noreply.github.com>
2026-07-14 15:00:30 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function Router() {
   return (
     <Switch>
       <Route path="/" component={Home} />
-      <Route component={Please enter a correct URL} />
+      <Route component={NotFound} />
     </Switch>
   );
 }
